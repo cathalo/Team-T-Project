b2819bb15ad00355f666fc4e5204794648041c63
Cathal O Maoldomhnaigh <cathalo@live.ie>
2016-11-14 10:48:33 +0000

    Add project files.

---
+++ TeamProjectA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamProjectA.MvcApplication" Language="C#" %>
---
+++ TeamProjectA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamProjectA.Startup))]
+namespace TeamProjectA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
